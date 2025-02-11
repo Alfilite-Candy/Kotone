677b16ee0e7071b8baa4f57fb3185d87c8433eaf
Alfilite Candy <alfilitecandy@gmail.com>
2025-02-11 11:55:03 +0800

    version 1.1

--- src/text.py
+++ src/text.py
@@ -8,39 +8,86 @@ from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import os
+import pickle
 
 # 全局变量
 driverfile_path = r".\msedgedriver.exe"
 username = ''
 password = ''
+low,high=2,5
 log_text = None
 stop_event = threading.Event()
+# 文件路径
+credentials_file = "credentials.pkl"
 
+# 保存学号和密码
+def save_credentials(username, password):
+    with open(credentials_file, "wb") as f:
+        pickle.dump({"username": username, "password": password}, f)
+
+# 加载学号和密码
+def load_credentials():
+    if os.path.exists(credentials_file):
+        with open(credentials_file, "rb") as f:
+            return pickle.load(f)
+    return {"username": "", "password": ""}
+
+def save_credentials(username, password):
+    with open(credentials_file, "wb") as f:
+        pickle.dump({"username": username, "password": password}, f)
+
+# 加载学号和密码
+def load_credentials():
+    if os.path.exists(credentials_file):
+        with open(credentials_file, "rb") as f:
+            return pickle.load(f)
+    return {"username": "", "password": ""}
 
 # GUI - 登录界面
 def get_login_info():
     def submit():
-        global username, password
+        global username, password,low ,high,respeed
         username = entry_username.get()
         password = entry_password.get()
+        if not entry_low.get():
+            low=entry_low.get()
+        if not entry_high.get():
+            high=entry_high.get()
+        if not entry_respeed.get():
+            respeed=entry_respeed.get()
         if not username or not password:
             messagebox.showwarning("输入错误", "学号和密码不能为空！")
             return
+        save_credentials(username,password)
         root.destroy()
         threading.Thread(target=main).start()
 
+    credentials = load_credentials()
     root = tk.Tk()
     root.title("登录信息")
 
     tk.Label(root, text="学号：").grid(row=0, column=0, padx=10, pady=10)
     entry_username = tk.Entry(root)
-    entry_username.grid(row=0, column=1, padx=10, pady=10)
+    entry_username.insert(0, credentials["username"])
+    entry_username.grid(row=0, column=1, columnspan=2, padx=10, pady=10)
 
     tk.Label(root, text="密码：").grid(row=1, column=0, padx=10, pady=10)
     entry_password = tk.Entry(root, show="*")
-    entry_password.grid(row=1, column=1, padx=10, pady=10)
+    entry_password.insert(0, credentials["password"])
+    entry_password.grid(row=1, column=1, columnspan=2, padx=10, pady=10)
 
-    tk.Button(root, text="确定", command=submit).grid(row=2, column=0, columnspan=2, pady=10)
+    tk.Label(root, text="预约楼层：").grid(row=2, column=0, padx=10, pady=10)
+    entry_low = tk.Entry(root,width=8)
+    entry_high=tk.Entry(root,width=8)
+    entry_low.grid(row=2, column=1, padx=5, pady=10)
+    entry_high.grid(row=2,column=2, padx=5, pady=10)
+
+    tk.Label(root, text="刷新速率（次/秒）：").grid(row=3, column=0, padx=10, pady=10)
+    entry_respeed = tk.Entry(root)
+    entry_respeed.grid(row=3, column=1, columnspan=2, padx=10, pady=10)
+
+    tk.Button(root, text="确定", command=submit,width=20).grid(row=4, column=0, columnspan=3, pady=10)
     root.mainloop()
 
 
@@ -95,79 +142,99 @@ def login(driver):
 
 # 预约座位的函数
 def reserve_seat(driver):
-    try:
-        X_path = '//*[@id="SeatScreening"]/div[5]/div/div/div[2]/button'
-        reserve_button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, X_path)))
-        reserve_button.click()
-
-        X_path = '//*[@id="SeatScreening"]/div[5]/div/div/div/div[1]/div[2]/div[1]/div[1]/div[2]'
-        reserve_button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, X_path)))
-        reserve_button.click()
-
-        X_path = '//*[@id="SeatScreening"]/div[5]/div/div/div/div[1]/div[2]/div[3]/div[1]'
-        reserve_button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, X_path)))
-        reserve_button.click()
-
-        try:
-            X_path = '//*[@id="SeatScreening"]/div[5]/div/div/div/div[2]/button[2]'
-            reserve_button = driver.find_element(By.XPATH, X_path)
-            reserve_button.click()
-        except Exception :
-            log_message("当前状态无法预约")
-            return False
-
-        X_path = '//*[@id="SeatScreening"]/div[5]/div/div/div/div[3]/div/div/div/div[1]/div'
-        confirm_button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, X_path)))
-
-
-        if confirm_button.text == '预约成功':
-            return True
-        else:
-            return False
-    except Exception as e:
-        log_message(f"预约座位时发生错误: {e}")
+#     try:
+#         X_path = '//*[@id="SeatScreening"]/div[5]/div/div/div[2]/button'
+#         reserve_button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, X_path)))
+#         reserve_button.click()
+#
+#         X_path = '//*[@id="SeatScreening"]/div[5]/div/div/div/div[1]/div[2]/div[1]/div[1]/div[2]'
+#         reserve_button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, X_path)))
+#         reserve_button.click()
+#
+#         X_path = '//*[@id="SeatScreening"]/div[5]/div/div/div/div[1]/div[2]/div[3]/div[1]'
+#         reserve_button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, X_path)))
+#         reserve_button.click()
+#
+#         try:
+#             X_path = '//*[@id="SeatScreening"]/div[5]/div/div/div/div[2]/button[2]'
+#             reserve_button = driver.find_element(By.XPATH, X_path)
+#             reserve_button.click()
+#         except Exception :
+#             log_message("当前状态无法预约")
+#             return False
+#
+#         X_path = '//*[@id="SeatScreening"]/div[5]/div/div/div/div[3]/div/div/div/div[1]/div'
+#         confirm_button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, X_path)))
+#
+#
+#         if confirm_button.text == '预约成功':
+#             return True
+#         else:
+#             return False
+#     except Exception as e:
+#         log_message(f"预约座位时发生错误: {e}")
         return False
 
 # 监控
 def monitor_area(driver):
     seat_areas = ["二层南", "二层北", "三层东", "三层南", "三层北", "四层东", "四层南", "四层西", "四层北", "五层东"]
     flag=0
+    time_signal=[1]*10
     no_seats = [False] * 10
+    if low==2:
+        l=0
+    elif low==3:
+        l=2
+    elif low==4:
+        l=5
+    else:
+        l=9
+    if high==2:
+        h=1
+    elif high==3:
+        h=4
+    elif high==4:
+        h=8
+    else:
+        h=9
     while flag==0:
         try:
-            for i in range(10):
+            for i in range(l,h+1):
                 area_name=seat_areas[i]
-                if not is_within_allowed_time(area_name):
+                if (not is_within_allowed_time(area_name)) and time_signal[i]:
                     log_message(f"{area_name} 当前不在允许的预约时间，跳过...")
-                    continue
+                    time_signal[i]=0
+                else:
+                    log_message(f"{area_name}当前在允许的预约时间，开始监控...")
+            for i in range(l,h+1):
+                if time_signal[i]:
+                    area_xpath = f"//*[@id='SeatScreening']/div[3]/div[2]/div/div/div[3]/div[1]/div[2]/div[{i + 1}]/div/div[2]/div[1]/div[2]/span[2]/b"
+                    try:
+                        area_div = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, area_xpath)))
+                        remaining_seats = int(area_div.text)
+                        log_message(f"{seat_areas[i]} 剩余座位：{remaining_seats}")
 
-                area_xpath = f"//*[@id='SeatScreening']/div[3]/div[2]/div/div/div[3]/div[1]/div[2]/div[{i + 1}]/div/div[2]/div[1]/div[2]/span[2]/b"
-                try:
-                    area_div = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, area_xpath)))
-                    remaining_seats = int(area_div.text)
-                    log_message(f"{seat_areas[i]} 剩余座位：{remaining_seats}")
+                        if remaining_seats > 0:
+                            if i != 0:
+                                reserve_button_xpath = f'//*[@id="SeatScreening"]/div[3]/div[2]/div/div/div[3]/div[1]/div[2]/div[{i + 1}]/div/div[2]/div[3]/button'
+                                reserve_button = WebDriverWait(driver, 5).until(
+                                    EC.presence_of_element_located((By.XPATH, reserve_button_xpath)))
+                                reserve_button.click()
 
-                    if remaining_seats > 0:
-                        if i != 0:
-                            reserve_button_xpath = f'//*[@id="SeatScreening"]/div[3]/div[2]/div/div/div[3]/div[1]/div[2]/div[{i + 1}]/div/div[2]/div[3]/button'
-                            reserve_button = WebDriverWait(driver, 5).until(
-                                EC.presence_of_element_located((By.XPATH, reserve_button_xpath)))
-                            reserve_button.click()
-
-                        if reserve_seat(driver):
-                            log_message(f"{area_name} 预约成功，停止监控。")
-                            return  # 成功预约，停止监控
+                            if reserve_seat(driver):
+                                log_message(f"{area_name} 预约成功，停止监控。")
+                                return  # 成功预约，停止监控
+                            else:
+                                log_message(f"{area_name} 预约失败，继续监控。")
+                                driver.back()
+                                info_refresh(driver, 0.8)
+                                no_seats[i] = True  # 标记当前区域没有座位
                         else:
-                            print(f"{area_name} 预约失败，继续监控。")
-                            driver.back()
-                            info_refresh(driver, 0.8)
+                            log_message(f"{area_name} 没有空余座位，继续监控...")
                             no_seats[i] = True  # 标记当前区域没有座位
-                    else:
-                        log_message(f"{area_name} 没有空余座位，继续监控...")
-                        no_seats[i] = True  # 标记当前区域没有座位
-                except Exception as e:
-                    log_message(f"监控区域 {area_name} 时发生错误: {e}")
-                    info_refresh(driver,1)
+                    except Exception as e:
+                        log_message(f"监控区域 {area_name} 时发生错误: {e}")
+                        info_refresh(driver,1)
 
             # 当所有区域都没有座位时，刷新页面
             if no_seats[0] and no_seats[1]:
